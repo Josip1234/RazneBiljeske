5a5b3d585b363b8ef9f6fe1da5ff75f3e8e410c5
Josip1234 <jbosnjak3@gmail.com>
2025-09-06 22:06:50 +0200

    Dodane datoteke

---
+++ Web stranice i konfiguracijske postavke/Web stranice/www/html/ost1.lan/public_html/prva.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Josip Bošnjak</title>
+</head>
+<body>
+
+<h1>Moje ime je Josip Bošnjak</h1>
+<p>Živim u Požegi. Imam 32 godine. Trenutno nezaposlen. Upisao sam tečaj u Algebri. Pohađam program za stjecanje djelomične kvalifikacije
+za backend developera. Nadam se da ću položiti. Želim vam svima ugodan dan!</p>
+<?php echo "<h2>Važna obavijest! Položio sam orvi parcijalni ispit održanog u četvrtak prošli tjedan. Nisam dobio postotak, 
+ali mislim da sam riješio negdje oko 70-80%.</h2>";  ?>
+
+</body>
+</html>
